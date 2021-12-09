7260bd94e9b20bb568a3380a8757256aac8f35d9
Bruna Topan <brutopanb@gmail.com>
2021-12-09 18:29:20 -0300

    Preparando review

--- src/helpers/getRandomInt.js
+++
@@ -1,7 +0,0 @@
-export default function getRandomInt(min, max) {
-    const minRounded = Math.ceil(min);
-    const maxRounded = Math.floor(max);
-    return Math.floor(
-        Math.random() * (maxRounded - minRounded + 1) + minRounded
-    );
-}
